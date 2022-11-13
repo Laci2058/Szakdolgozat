b80ba8ee7a4a77d22884bb64d016fd72a37417ab
Laci2058 <mezeilacko@gmail.com>
2022-11-13 16:49:56 +0100

    Beviteli mező hozzáadása a webes felülethez, github api lekérdezés megvalósítása webes felületlen keresztül

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 import requests
 
 app = Flask(__name__)
@@ -6,33 +6,39 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():  # put application's code here
-    issue_url = f"https://api.github.com/repos/CsharptutorialHungary/egyetemikurzus-2022/issues"
-    all_issue = requests.get(issue_url).json()
-    commit_url = f"https://api.github.com/repos/CsharptutorialHungary/egyetemikurzus-2022/commits"
-    all_commit = requests.get(commit_url).json()
+    """ issue_url = f"https://api.github.com/repos/CsharptutorialHungary/egyetemikurzus-2022/issues"
+     all_issue = requests.get(issue_url).json()
+     commit_url = f"https://api.github.com/repos/CsharptutorialHungary/egyetemikurzus-2022/commits"
+     all_commit = requests.get(commit_url).json()
 
-    issue_id = []  # issuek id-jének tömbje
-    issue_assignees = []  # issue-hoz rendelt fejlesztők adatai
-    commit_author = []  # commitok szerzőinek a neve
+     issue_id = []  # issuek id-jének tömbje
+     issue_assignees = []  # issue-hoz rendelt fejlesztők adatai
+     commit_author = []  # commitok szerzőinek a neve
 
-    # commit_list = []
+     for issue in all_issue:  # issuek id-jének és a hozzárendelt hallgatók lekérése
+         issue_id.append(issue["id"])
+         issue_assignees.append(issue["assignees"])
 
-    for issue in all_issue:  # issuek id-jének és a hozzárendelt hallgatók lekérése
-        issue_id.append(issue["id"])
-        issue_assignees.append(issue["assignees"])
+     for commit in all_commit:
+         commit_author.append(commit["commit"]["author"]["name"])
 
-    for commit in all_commit:
-        commit_author.append(commit["commit"]["author"]["name"])    # !!!!ellenőrizni hogy author nem null!!!!
-    #   commit_list.append(commit["commit"]["author"])
+     commits_per_author = {i: commit_author.count(i) for i in
+                           commit_author}  # commitok szerzőinek a neve és commitjainak száma
 
-    commits_per_author = {i: commit_author.count(i) for i in
-                          commit_author}  # commitok szerzőinek a neve és commitjainak száma
+     number_of_issues = len(issue_id)  # issuek száma
+     number_of_commits = len(commit_author)  # commitok száma """
 
-    number_of_issues = len(issue_id)  # issuek száma
-    number_of_commits = len(commit_author)  # commitok száma
+    return render_template("index.html")
 
-
-    return all_commit # !!!!commitok száma nem pontos, mert üres author-ral is számolja!!!!
+@app.route('/result',methods = ['POST','GET'])
+def result():
+    output = request.form.to_dict()
+    name = output["api_link"].split('/')
+    """
+    all_issue = requests.get(f"https://api.github.com/repos/{name[-2]}/{name[-1]}/issues").json()
+    all_commit = requests.get(f"https://api.github.com/repos/{name[-2]}/{name[-1]}/commits").json()
+    """
+    return render_template("index.html",name=name)
 
 
 if __name__ == '__main__':
